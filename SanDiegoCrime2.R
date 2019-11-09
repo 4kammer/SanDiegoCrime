423f78a623bf906020ac9496a66a4ace94b6736f
Kam <5490773+4kammer@users.noreply.github.com>
2019-11-08 18:59:22 -0800

    Add files via upload
    additional pivot tables for most crimes in the dataset along with table of cities with most reported crimes in descending order

--- SanDiegoCrime2.R
+++ SanDiegoCrime2.R
@@ -1,3 +1,4 @@
+# author: @KamZardouzian
 # 6 Month Crime Stats for San Diego (Oct 14, 2019)
 # Data source: SANDAG, https://www.sandiego.gov/police/services/statistics, 
 # Automated Regional Justice Information System (ARJIS)
@@ -5,7 +6,8 @@
 library(dplyr)
 library(downloader)
 library(data.table)
-
+library(lubridate)
+library(radiant)
 
 # Upload Crime Data text file (in Zip format) from source, unzip, & save as crime using fread
 
@@ -19,7 +21,6 @@ crime_work <- crime
 
 # activityDate is character. Needs to be changed to data, we'll also get rid of time & just keep date.
 # change activityDate from character to data & time format so we can group crime by date
-library(lubridate)
 crime_work$activityDate <- mdy_hms(crime_work$activityDate,tz=Sys.timezone())
 crime_work$activityDate <- as.Date(crime_work$activityDate)
 
@@ -45,5 +46,40 @@ dtab(result, dec = 0, pageLength = -1) %>% render()
 # plot the pivot table, grouping crimes in Encinitas in descending order
 plot(result)
 
+# Create a similar pivot table for crimes in all of San Diego County, crime_work contains all data
+View(crime_work %>%
+       group_by(CM_LEGEND) %>%
+       arrange(CM_LEGEND)
+     )
 
+# create pivot table for San Diego County crimes descending by most number of crimes
+result2 <- pivotr(
+  crime_work, 
+  cvars = "CM_LEGEND", 
+  tabsort = "desc(n_obs)", 
+  nr = Inf
+)
 
+# create dynamic data table 
+dtab(result2, dec = 0, pageLength = -1) %>% render()
+
+# plot the pivot table, grouping crimes in Encinitas in descending order
+plot(result2)
+
+# create pivot table to rank communities in San Diego County by most number of crimes
+View(crime_work %>%
+       group_by(CM_LEGEND) %>%
+       arrange(community)
+)
+
+result3 <- pivotr(
+  crime_work, 
+  cvars = "community", 
+  tabsort = "desc(n_obs)", 
+  nr = Inf
+)
+
+# create dynamic data table 
+dtab(result3, dec = 0, pageLength = -1) %>% render()
+
+# plot results is not useful
